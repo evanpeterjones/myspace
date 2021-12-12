f218dc198d17ac04273135f425b09a77635005a2
Evan Jones <evan.peter.jones@gmail.com>
2021-12-12 17:06:20 -0600

    updated build script

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class myspace extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            Welcome
           </Text>
         </View>
       </View>
