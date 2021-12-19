3a24506b3c45383f05128c41bcd225e6d7010c9b
Evan Jones <evanpeterjones@gmail.com>
2021-12-18 19:13:59 -0600

    playing with rain animation
    learning how the animation would work with generic spheres, the new raindrop.obj isn't loading properly, but the rain scene does, so that's a bit odd. So much to learn but holy crap does aframe cover a lot of what you need.

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import 'aframe';
+
+function RainDrop (props) {
+    return (
+            <a-sphere position={props.position} radius=".1" color="#8CDFE8"></a-sphere>
+    );
+}
+
+function random(x) {
+    return Math.random() * x;
+}
+
+function App() {
+    let raindrops = [];
+
+    for (var i = 0; i < 400; i++) {
+        raindrops.push(<RainDrop position={(random(40)-20).toString() + " " +
+                                           (random(40)-20).toString() +  " " + 
+                                           (random(40)-20).toString()}/>);
+    }
+
+    return (
+        <div className="App">
+            <a-scene>
+        <a-asset id="drop" src="raindrop.obj"></a-asset>
+            <a-asset id="rain" src="rain.obj"></a-asset>
+            <a-obj-model src="#rain" position="-2 -10 -5"></a-obj-model>
+            {raindrops}
+        </a-scene>
+            </div>
+    );
+}
+
+export default App;
